2b5bcf84d4b646c545c15ddbbf7c4f7726c846cb
Neil <alegreneilkervey38@gmail.com>
2026-04-07 23:23:26 +0800

    changes

--- backend_profiling/scripts/00-laravel-deploy.sh
+++ backend_profiling/scripts/00-laravel-deploy.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-set -e
-
-echo "Running composer..."
+echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
-
-echo "Clearing old cache..."
-php artisan config:clear
-php artisan cache:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -17,5 +11,5 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-echo "Seeding admin user..."
+echo "Seeding admin..."
 php artisan db:seed --class=AdminSeeder --force
