a8dc2547d6fc91ffa327f9b2b47e8d23684d1e3f
Neil <alegreneilkervey38@gmail.com>
2026-04-08 10:04:53 +0800

    changes in cors

--- backend_profiling/config/cors.php
+++ backend_profiling/config/cors.php
@@ -3,13 +3,13 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => array_filter([
+    'allowed_origins' => array_values(array_filter([
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5174',
         env('FRONTEND_URL'),
-    ]),
+    ])),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
