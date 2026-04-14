eef394bafa8f83c1cd3f681720ea8a6a9f2eaf25
Neil <alegreneilkervey38@gmail.com>
2026-04-14 15:01:05 +0800

    fix duplicate CORS headers - remove from server block and Laravel middleware

--- backend_profiling/bootstrap/app.php
+++ backend_profiling/bootstrap/app.php
@@ -12,9 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
-        ]);
+        // CORS handled by nginx
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
