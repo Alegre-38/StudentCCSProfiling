76f5088c7d8c0c4189a8b6b9e41527e2d41dd894
Neil <alegreneilkervey38@gmail.com>
2026-04-14 14:30:00 +0800

    remove config:cache to fix persistent CORS issue

--- backend_profiling/scripts/00-laravel-deploy.sh
+++ backend_profiling/scripts/00-laravel-deploy.sh
@@ -2,13 +2,10 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Clearing caches..."
+echo "Clearing all caches..."
 php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
-
-echo "Caching config..."
-php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
